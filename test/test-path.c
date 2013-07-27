ca406883a5c2054dab254995d0a657c0bcce1556
Bo-cheng Jan <school510587@yahoo.com.tw>
2013-07-27 21:28:38 +0800

    Clear cancelled macros.

--- test/test-path.c
+++ test/test-path.c
@@ -29,15 +29,7 @@ int find_path_by_files(
 	size_t output_len );
 
 static const char *FILES[] = {
-	CHAR_FILE,
-#ifdef USE_BINARY_DATA
-	CHAR_INDEX_BEGIN_FILE,
-	CHAR_INDEX_PHONE_FILE,
-#else
-	CHAR_INDEX_FILE,
-#endif
 	DICT_FILE,
-	PH_INDEX_FILE,
 	PHONE_TREE_FILE,
 	SYMBOL_TABLE_FILE,
 	SOFTKBD_TABLE_FILE,
