de61ef3ed248fe168967131d8b15f1af13f134ea
Bo-cheng Jan <school510587@yahoo.com.tw>
2013-08-14 01:03:37 +0800

    Let gen_IM.c read cin files.

--- src/tools/gen_IM_data.c
+++ src/tools/gen_IM_data.c
@@ -1,4 +1,27 @@
+#include <libgen.h> /* For basename(). */
 #include <stdio.h>
-int main(){
-}
+#include <stdlib.h>
+#include <string.h>
 
+#include "build_tool.h"
+
+#define CIN_EXTENSION ".cin"
+
+int main(int argc, char *argv[])
+{
+	int cin_path_id;
+
+	for(cin_path_id = 1; cin_path_id < argc; cin_path_id++){
+		size_t l = strlen( argv[cin_path_id] );
+		if(l>4 && !strcmp( &argv[cin_path_id][l-4], CIN_EXTENSION) ) break;
+	}
+
+	if( cin_path_id >= argc) {
+		fprintf(stderr, "Usage: %s <cin_filename>\n", argv[0]);
+		exit(-1);
+	}
+
+	read_IM_cin( argv[cin_path_id] );
+
+	return 0;
+}
