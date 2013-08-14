c86e1bfa4ff3eba01639ec093a2b01c826d90e4f
Bo-cheng Jan <school510587@yahoo.com.tw>
2013-08-15 00:58:47 +0800

    Let gen_IM_data link to libporting_layer.la
    	Issue: porting_layer should be a subdirectory of src/

--- src/tools/gen_IM_data.c
+++ src/tools/gen_IM_data.c
@@ -10,6 +10,10 @@
 
 int main(int argc, char *argv[])
 {
+	plat_mmap dict_map;
+	long dict_size;
+	size_t offset=0, csize;
+	const char *dict;
 	int cin_path_id;
 
 	for(cin_path_id = 1; cin_path_id < argc; cin_path_id++){
@@ -27,5 +31,16 @@ int main(int argc, char *argv[])
 	/* Go to data/, where the exe is. */
 	chdir(dirname( argv[0] ));
 
+	plat_mmap_set_invalid(&dict_map);
+	dict_size = plat_mmap_create(&dict_map, DICT_FILE, FLAG_ATTRIBUTE_READ);
+	csize = dict_size;
+	dict=(const char*)plat_mmap_set_view(&dict_map, &offset, &csize);
+
+	if( !dict ) {
+		fprintf(stderr, "%s: Error reading system dictionary.\n", argv[0]);
+		exit(-1);
+	}
+
+	plat_mmap_close(&dict_map);
 	return 0;
 }
