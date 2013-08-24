899ee459ef10181a7011be87887fb25e17898e16
Bo-cheng Jan <school510587@yahoo.com.tw>
2013-08-25 01:13:46 +0800

    Add a file to contain test cases for non-phonetic IM's.

---
+++ test/test-non-bopomofo-IM.c
@@ -0,0 +1,26 @@
+/**
+ * test-non-bopomofo-IM.c
+ *
+ * Copyright (c) 2013
+ *      libchewing Core Team. See ChangeLog for details.
+ *
+ * See the file "COPYING" for information on usage and redistribution
+ * of this file.
+ */
+/**
+ * @file test-non-bopomofo-IM.c
+ * @brief Test cases of newly built APIs for non-phonetic IM's.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "chewing.h"
+#include "testhelper.h"
+
+
