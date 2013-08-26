c6addfcc18ef3caad98a5bfc5e5c779a9de679cf
Bo-cheng Jan <school510587@yahoo.com.tw>
2013-08-26 23:46:19 +0800

    Modify TreeFindPhrase() for new TreeType.
    	Its return value becomes the index of a parent, whose children are
    	leaves of phrases.
    
    Conflicts:
    
    	src/tree.c

--- src/tree.c
+++ src/tree.c
@@ -249,22 +249,22 @@ int TreeFindPhrase( ChewingData *pgdata, int begin, int end, const uint16_t *pho
 	tree_p = 0;
 	for ( i = begin; i <= end; i++ ) {
 		for (
-			child = pgdata->static_data.tree[ tree_p ].child_begin;
-			child != -1 && child <= pgdata->static_data.tree[ tree_p ].child_end;
+			child = pgdata->static_data.tree[ tree_p ].child.begin;
+			child < pgdata->static_data.tree[ tree_p ].child.end;
 			child++ ) {
 
 			assert(0 <= child && child * sizeof(TreeType) < pgdata->static_data.tree_size);
-			if ( pgdata->static_data.tree[ child ].phone_id == phoneSeq[ i ] )
+			if ( pgdata->static_data.tree[ child ].key == phoneSeq[ i ] )
 				break;
 		}
 		/* if not found any word then fail. */
-		if ( child == -1 || child > pgdata->static_data.tree[ tree_p ].child_end )
+		if ( child >= pgdata->static_data.tree[ tree_p ].child.end )
 			return -1;
 		else {
 			tree_p = child;
 		}
 	}
-	return pgdata->static_data.tree[ tree_p ].phrase_id;
+	return tree_p;
 }
 
 static void AddInterval(
